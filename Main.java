46851fb706aeb83607b366b1a8e3ef2aafd2f6c1
aymenimos <aarreeggwweell@gmail.com>
2022-12-28 12:14:49 +0300

    add

---
+++ Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Node q= new Node();
+        actions a=new actions();
+        a.name(12);
+        a.name(23);
+        a.name(2);
+        a.name(45);
+        a.name(4);
+        a.name(1);
+        a.show();
+    }
+        
+    }
---
+++ actions.java
@@ -0,0 +1,41 @@
+
+
+class Node{
+    int data;
+    Node next;
+    }
+    
+    
+    
+    
+    public class actions {
+        Node head;
+        Node prev;
+        public void name(int number) {
+        Node newnode= new Node();
+        newnode.data=number;
+            if (head==null) {
+                head=newnode;
+            } else {
+                Node pointer=head;
+                while (pointer!=null) {
+                  
+                    pointer=pointer.next;
+    
+                    
+                }
+                pointer=newnode;
+            }
+        }
+        public void show() {
+        Node pointer=head;
+        while (pointer!=null) {
+          
+            System.out.println(pointer.data);
+             pointer=pointer.next; 
+        }
+        
+        }
+    
+    }
+    
