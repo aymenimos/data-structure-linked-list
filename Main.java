1a2177b9c4c92d1047d7a41e7c4b8900b41c5d03
aymenimos <aarreeggwweell@gmail.com>
2022-12-28 13:16:13 +0300

    add to insert

--- Main.java
+++ Main.java
@@ -3,14 +3,14 @@ public class Main {
     public static void main(String[] args) {
         Node q= new Node();
         actions a=new actions();
-        a.name(12);
-        a.name(23);
-        a.name(2);
-        a.name(45);
+        a.insert(12);
+        a.insert(23);
+        a.insert(2);
+        a.insert(45);
 
-        a.name(45);
-        a.name(4);
-        a.name(1);
+        a.insert(45);
+        a.insert(4);
+        a.insert(1);
         a.show();
     }
         
